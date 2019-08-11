71ec5fc4f3c8133611a787ab88f421d8e36d98ae
sporchia <print@blindchaos.net>
2019-08-11 17:17:12 -0400

    The App should contain a game board

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Board from "./GameBoard";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+const wrapper = shallow(<App />);
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("has a game board", () => {
+  expect(wrapper.find(Board)).toHaveLength(1);
 });
