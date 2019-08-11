8c23f1e1a6a94f2b292dd1274ac215ae286cef92
sporchia <print@blindchaos.net>
2019-08-11 18:41:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import GameBoard from "./GameBoard";
 
 class App extends React.Component {
+  state = {
+    currentPlayer: "X"
+  };
+
   render() {
     return (
       <div className="App">
