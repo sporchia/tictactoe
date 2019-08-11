665692f3318fb6412faef0dc5cfcca9c8add408e
sporchia <print@blindchaos.net>
2019-08-11 19:25:26 -0400

    default owner is null

--- src/Square.test.js
+++ src/Square.test.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Square from "./Square";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+const wrapper = shallow(<Square />);
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Square />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("starts with no owner", () => {
+  expect(wrapper.prop("owner")).toBe(null);
 });
