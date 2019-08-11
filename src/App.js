d13b67e4f1c487145af5354d19add1384ebb8d8d
sporchia <print@blindchaos.net>
2019-08-11 17:17:28 -0400

    place game board in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import GameBoard from "./GameBoard";
 
 class App extends React.Component {
   render() {
-    return <div className="App">Starting Point</div>;
+    return (
+      <div className="App">
+        <GameBoard />
+      </div>
+    );
   }
 }
 
