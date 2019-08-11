7bf1a322c6cbf18c4aaea13e2a57dc55fb6ebbf8
sporchia <print@blindchaos.net>
2019-08-11 17:16:09 -0400

    basic game board object

---
+++ src/GameBoard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GameBoard() {
+  return <div className="GameBoard" />;
+}
+
+export default GameBoard;
--- src/GameBoard.test.js
+++ src/GameBoard.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import GameBoard from "./GameBoard";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
