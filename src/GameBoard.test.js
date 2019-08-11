97af4412d1c971314caad837aa1dd49e0fac6f78
sporchia <print@blindchaos.net>
2019-08-11 18:00:06 -0400

    we want 9 squares in the board

--- src/GameBoard.test.js
+++ src/GameBoard.test.js
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import GameBoard from "./GameBoard";
+import Square from "./Square";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+const wrapper = shallow(<GameBoard />);
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<GameBoard />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("should contain 9 squares", () => {
+  expect(wrapper.find(Square)).toHaveLength(9);
 });
