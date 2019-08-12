a8a144869804b391285b8e5274ba58cb620819ad
sporchia <print@blindchaos.net>
2019-08-11 21:03:03 -0400

    beginning of reset functions

--- src/App.test.js
+++ src/App.test.js
@@ -7,8 +7,6 @@ import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
-const wrapper = shallow(<App />);
-
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
@@ -16,14 +14,17 @@ it("renders without crashing", () => {
 });
 
 it("has a game board", () => {
+  const wrapper = shallow(<App />);
   expect(wrapper.find(Board)).toHaveLength(1);
 });
 
 it("player X should go first", () => {
+  const wrapper = shallow(<App />);
   expect(wrapper.state().currentPlayer).toBe("X");
 });
 
 it("changes player when changePlayer is called", () => {
+  const wrapper = shallow(<App />);
   wrapper.instance().changePlayer();
   expect(wrapper.state().currentPlayer).toBe("O");
 });
@@ -31,4 +32,11 @@ it("changes player when changePlayer is called", () => {
 it("should let the players know who’s turn it is", () => {
   const wrapper = shallow(<App />);
   expect(wrapper.text()).toMatch(/It is currently X’s turn/);
+});
+
+it("should set the starting player back to X on reset", () => {
+  const wrapper = shallow(<App />);
+  wrapper.instance().changePlayer();
+  wrapper.instance().reset();
+  expect(wrapper.state().currentPlayer).toBe("X");
 });
